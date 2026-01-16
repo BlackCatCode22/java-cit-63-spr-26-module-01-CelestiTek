0c9df5d28b6a58737ad94342f05b9efab86d8e51
Jose Arroyo <1047719@MY.SCCCD.EDU>
2026-01-16 11:33:48 -0800

    Hello world program complete

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+// HelloWorld.java
+// A simple Java program that prints "Hello World!" to the console.
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Output the text string to the console
+        System.out.println("Hello World!");
+    }
+}
